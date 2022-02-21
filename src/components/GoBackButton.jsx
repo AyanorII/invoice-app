a1b7cabd6ee3c4d50eda9e3bebbf88634c163482
Ayanori Toyoda <rodrigotoyoda@outlook.com>
2022-02-21 23:36:00 +0900

    added go back button

---
+++ src/components/GoBackButton.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+import styled from "styled-components";
+import { NavLink } from "react-router-dom";
+import arrow from "../assets/icon-arrow-left.svg";
+
+const Button = styled(NavLink)`
+  color: ${(props) => props.theme.text.primary};
+  font-size: 0.75rem;
+  font-weight: 700;
+  left: 0.5rem;
+  position: relative;
+  text-decoration: none;
+
+  &::before {
+    content: url(${arrow});
+    left: -1rem;
+    position: absolute;
+    top: 50%;
+    transform: translateY(-50%);
+    transition: all 0.35s;
+  }
+
+  &:hover::before {
+    left: -1.5rem;
+  }
+`;
+
+function GoBackButton() {
+  return <Button to="/">Go back</Button>;
+}
+
+export default GoBackButton;
