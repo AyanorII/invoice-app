74112c09f033fe27cddebb552ca7d6fd1e317110
Ayanori Toyoda <rodrigotoyoda@outlook.com>
2022-02-22 00:31:46 +0900

    change padding to margin on container

--- src/components/Container.jsx
+++ src/components/Container.jsx
@@ -2,11 +2,11 @@ import React from "react";
 import styled from "styled-components";
 
 const StyleContainer = styled.div`
-  padding-inline: 8.5%;
+  margin-inline: 8.5%;
   margin-top: 2rem;
 
   @media (min-width: 768px) {
-    padding-inline: 6.25%;
+    margin-inline: 6.25%;
   }
 `;
 
