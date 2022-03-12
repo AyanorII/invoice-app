a66a935f00f750d4d66e5d95fe9c5b987e94d552
Ayanori Toyoda <rodrigotoyoda@outlook.com>
2022-03-12 19:48:47 +0900

    created FormSection component

---
+++ client/src/components/form/FormSection.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import styled from 'styled-components';
+
+const StyledSection = styled.div`
+  margin-top: 2rem;
+  display: grid;
+  grid-template-columns: 1fr 1fr 1fr;
+  gap: 1.5rem;
+`;
+
+function FormSection({ children }) {
+  return (
+    <StyledSection>{children}</StyledSection>
+  )
+}
+export default FormSection
