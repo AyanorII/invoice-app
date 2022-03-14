132c4d70dff013644b39b07825eb8dd809612fba
Ayanori Toyoda <rodrigotoyoda@outlook.com>
2022-03-14 19:13:56 +0900

    added onSubmit to invoice/new form

--- client/src/components/form/Form.jsx
+++ client/src/components/form/Form.jsx
@@ -1,26 +1,38 @@
 import React from "react";
+import styled from "styled-components";
 import { useForm, FormProvider } from "react-hook-form";
 import BillFrom from "./BillFrom";
 import BillTo from "./BillTo";
 import Dates from "./Dates";
+import FormFooter from "./FormFooter";
 import ItemList from "./ItemList";
 import ProductDescription from "./ProductDescription";
+
+const StyledForm = styled.form`
+  padding-bottom: 5rem;
+`;
 
 function Form() {
   const methods = useForm();
   const onSubmit = (data) => {
-    console.log(data);
+    fetch("http://localhost:5001/invoices/create", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(data),
+    }).then((res) => console.log(res));
   };
   return (
     <FormProvider {...methods}>
-      <form onSubmit={methods.handleSubmit(onSubmit)}>
+      <StyledForm
+        onSubmit={methods.handleSubmit(onSubmit)}
+      >
         <BillFrom />
         <BillTo />
         <Dates />
         <ProductDescription />
         <ItemList />
-        <button>submit</button>
-      </form>
+        <FormFooter />
+      </StyledForm>
     </FormProvider>
   );
 }
