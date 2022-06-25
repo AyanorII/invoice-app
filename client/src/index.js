5f52e02d70ab010372d511f5572f6ecf2196bd67
Ayanori Toyoda <rodrigotoyoda@outlook.com>
2022-06-25 16:05:48 +0900

    fixed google analytics

--- client/src/index.js
+++ client/src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import GA4React from "ga-4-react";
 
 ReactDOM.render(
   <React.StrictMode>
@@ -15,3 +16,12 @@ ReactDOM.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
+try {
+  setTimeout((_) => {
+    const ga4react = new GA4React("G-J67QQ0EGSQ");
+    ga4react.initialize().catch((err) => console.error(err));
+  }, 4000);
+} catch (err) {
+  console.error(err);
+}
